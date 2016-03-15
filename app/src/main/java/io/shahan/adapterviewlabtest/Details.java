40b54884842269595d2ac8bcd11767bc681566b8
Shahan Shawkat <shahan.shawkat@gmail.com>
2016-03-15 20:04:41 +0600

    Initial commit

---
+++ app/src/main/java/io/shahan/adapterviewlabtest/Details.java
@@ -0,0 +1,18 @@
+package io.shahan.adapterviewlabtest;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+
+public class Details extends Activity {
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.details);
+
+
+        //Intent newActivity1=new Intent();
+        //setResult(RESULT_OK, newActivity1);
+        //finish();
+    }
+
+}
---
+++ app/src/main/java/io/shahan/adapterviewlabtest/MainActivity.java
@@ -0,0 +1,56 @@
+package io.shahan.adapterviewlabtest;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
+import android.widget.Toast;
+import android.app.Activity;
+
+public class MainActivity extends Activity {
+    ListView list;
+    String[] web = {
+            "Someone Like You",
+            "Space Bound",
+            "Stranger In Moscow",
+            "Love The Way You Lie",
+            "Khwaja Mere Khwaja",
+            "All My Days",
+            "Madness"
+    } ;
+    Integer[] imageId = {
+            R.drawable.image1,
+            R.drawable.image2,
+            R.drawable.image3,
+            R.drawable.image4,
+            R.drawable.image5,
+            R.drawable.image6,
+            R.drawable.image7
+
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        CustomList adapter = new
+                CustomList(MainActivity.this, web, imageId);
+        list=(ListView)findViewById(R.id.list);
+        list.setAdapter(adapter);
+        list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view,
+                                    int position, long id) {
+                    Intent newActivity = new Intent(MainActivity.this, Details.class);
+                        startActivity(newActivity);
+                }
+
+
+        });
+
+    }
+
+}
